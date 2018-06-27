8ae554bc77329d6a3acd2df867eb43364f2260d3
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2018-06-27 14:24:55 -0700

    Sort tools

--- help.go
+++ help.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 // Help prints tool listing
 func Help(args []string) {
-	str := "Tools:\n"
+	tools := make([]string, 0, len(Dispatch))
 	for k := range Dispatch {
+		tools = append(tools, k)
+	}
+
+	str := "Tools:\n"
+	sort.Strings(tools)
+	for _, k := range tools {
 		str += " * " + k + "\n"
 	}
 	fmt.Println(str)
