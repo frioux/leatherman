160ab5e675719e6b32dff5140df28b6ec3d4060f
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-01-31 09:22:42 -0800

    Add ID to todos

--- internal/notes/notes.go
+++ internal/notes/notes.go
@@ -17,11 +17,12 @@ var bodyTemplate *template.Template
 func init() {
 	var err error
 	bodyTemplate, err = template.New("xxx").Parse(`---
-title: {{.}}
+title: {{.Message}}
 tags: [ private, inbox ]
+guid: {{.ID}}
 ---
 
- * {{.}}
+ * {{.Message}}
 
 `)
 	if err != nil {
@@ -29,20 +30,21 @@ tags: [ private, inbox ]
 	}
 }
 
-func body(message string) io.Reader {
+func body(message, id string) io.Reader {
 	buf := &bytes.Buffer{}
 
-	bodyTemplate.Execute(buf, message)
+	bodyTemplate.Execute(buf, struct{ Message, ID string }{message, id})
 
 	return buf
 }
 
 // Todo creates an item tagged inbox
 func Todo(cl *http.Client, tok, message string) error {
-	buf := body(message)
-
 	sha := sha1.Sum([]byte(message))
-	path := "/notes/content/posts/todo-" + hex.EncodeToString(sha[:]) + ".md"
+	id := hex.EncodeToString(sha[:])
+	path := "/notes/content/posts/todo-" + id + ".md"
+
+	buf := body(message, id)
 
 	up := dropbox.UploadParams{Path: path, Autorename: true}
 	if err := dropbox.Create(cl, tok, up, buf); err != nil {
