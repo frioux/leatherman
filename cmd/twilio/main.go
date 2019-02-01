5e1c8b9739c8e1144bd9e8b087d25e23957921a9
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-01-31 21:58:31 -0800

    Add twilio emulator

---
+++ cmd/twilio/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/frioux/amygdala/internal/twilio"
+)
+
+var endpoint, auth, message, from string
+
+func init() {
+	flag.StringVar(&endpoint, "endpoint", "http://localhost:8080/twilio", "endpoint to post to")
+	flag.StringVar(&auth, "auth", "xyzzy", "auth token")
+	flag.StringVar(&message, "message", "this is a test", "message to submit to amygdala")
+	flag.StringVar(&from, "from", "+15555555555", "cell message is from")
+}
+
+func main() {
+	flag.Parse()
+
+	vals := url.Values{
+		"Body": {message},
+		"From": {from},
+	}
+	r := strings.NewReader(vals.Encode())
+	req, err := http.NewRequest("POST", endpoint, r)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.ParseForm()
+	sig := twilio.GenerateMAC([]byte(auth), []byte(endpoint), req)
+	encodedSignature := base64.StdEncoding.EncodeToString(sig)
+
+	req.Header.Set("X-Twilio-Signature", encodedSignature)
+	r.Reset(vals.Encode())
+
+	cl := &http.Client{}
+	resp, err := cl.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp.Status)
+	if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
+		panic(err)
+	}
+}
--- internal/twilio/twilio.go
+++ internal/twilio/twilio.go
@@ -11,13 +11,13 @@ import (
 	"github.com/pkg/errors"
 )
 
-func CheckMAC(key, url []byte, r *http.Request) (bool, error) {
+func GenerateMAC(key, url []byte, r *http.Request) []byte {
 	var buf bytes.Buffer
 
 	buf.Write(url)
 
-	keys := make(sort.StringSlice, 0, len(r.Form))
-	for k := range r.Form {
+	keys := make(sort.StringSlice, 0, len(r.PostForm))
+	for k := range r.PostForm {
 		keys = append(keys, k)
 	}
 
@@ -25,14 +25,18 @@ func CheckMAC(key, url []byte, r *http.Request) (bool, error) {
 
 	for _, k := range keys {
 		buf.WriteString(k)
-		for _, v := range r.Form[k] {
+		for _, v := range r.PostForm[k] {
 			buf.WriteString(v)
 		}
 	}
 
 	mac := hmac.New(sha1.New, key)
 	mac.Write(buf.Bytes())
-	expectedMAC := mac.Sum(nil)
+	return mac.Sum(nil)
+}
+
+func CheckMAC(key, url []byte, r *http.Request) (bool, error) {
+	expectedMAC := GenerateMAC(key, url, r)
 	messageMAC, err := base64.StdEncoding.DecodeString(r.Header.Get("X-Twilio-Signature"))
 	if err != nil {
 		return false, errors.Wrap(err, "base64.Decode")
