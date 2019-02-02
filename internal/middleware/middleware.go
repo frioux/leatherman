a2d034461c94c7ab9674cd809fc5cd6acdc0ef41
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-02-02 14:33:00 -0800

    Improve accesslog

--- internal/middleware/middleware.go
+++ internal/middleware/middleware.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"io"
 	"net/http"
+	"time"
 
 	"encoding/json"
 )
@@ -17,6 +18,10 @@ func Adapt(h http.Handler, adapters ...Adapter) http.Handler {
 }
 
 type logline struct {
+	Time string
+	Type string
+
+	Duration   float64
 	Method     string
 	URL        string
 	UserAgent  string
@@ -31,10 +36,14 @@ func Log(logger io.Writer) Adapter {
 
 	return func(h http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
 
 			lw := &loggingResponseWriter{ResponseWriter: w}
 			defer func() {
 				e.Encode(logline{
+					Type:       "accesslog",
+					Time:       start.Format(time.RFC3339Nano),
+					Duration:   time.Now().Sub(start).Seconds(),
 					Method:     r.Method,
 					URL:        r.URL.String(),
 					UserAgent:  r.UserAgent(),
