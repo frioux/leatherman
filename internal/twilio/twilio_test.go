9ef6842db49bfb699421c5fe1bbc5d8b1cfd65e4
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-02-02 21:26:03 -0800

    Fix test

--- internal/twilio/twilio_test.go
+++ internal/twilio/twilio_test.go
@@ -10,19 +10,19 @@ import (
 
 func TestCheckMAC(t *testing.T) {
 	// Example from https://www.twilio.com/docs/usage/security#notes
-	ok, err := CheckMAC([]byte("12345"), []byte("https://mycompany.com/myapp.php?foo=1&bar=2"),
-		&http.Request{
-			Form: url.Values(map[string][]string{
-				"CallSid": []string{"CA1234567890ABCDE"},
-				"Caller":  []string{"+12349013030"},
-				"Digits":  []string{"1234"},
-				"From":    []string{"+12349013030"},
-				"To":      []string{"+18005551212"},
-			}),
-			Header: http.Header(map[string][]string{
-				"X-Twilio-Signature": []string{"0/KCTR6DLpKmkAf8muzZqo1nDgQ="},
-			}),
-		})
+	ok, err := CheckMAC([]byte("12345"), []byte("https://mycompany.com/myapp.php?foo=1&bar=2"), &http.Request{
+		PostForm: url.Values(map[string][]string{
+			"CallSid": {"CA1234567890ABCDE"},
+			"Caller":  {"+12349013030"},
+			"Digits":  {"1234"},
+			"From":    {"+12349013030"},
+			"To":      {"+18005551212"},
+		}),
+		Header: http.Header(map[string][]string{
+			"X-Twilio-Signature": {"0/KCTR6DLpKmkAf8muzZqo1nDgQ="},
+			"Content-Type":       {"application/x-www-form-urlencoded"},
+		}),
+	})
 
 	assert.True(t, ok)
 	assert.NoError(t, err)
