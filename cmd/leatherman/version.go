3510b0a06cb1c3e31a1c1bf25c0435d3ce0a65ad
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-02-25 21:57:45 -0800

    upgrade to go1.12

--- cmd/leatherman/version.go
+++ cmd/leatherman/version.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"runtime"
+	"runtime/debug"
 )
 
 var version, when, who, where string
@@ -12,6 +13,19 @@ var version, when, who, where string
 func Version(args []string, _ io.Reader) error {
 	fmt.Printf("Leatherman built from %s on %s by %s@%s with %s\n",
 		version, when, who, where, runtime.Version())
+
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return nil
+	}
+
+	for _, dep := range bi.Deps {
+		fmt.Printf("%s@%s (%s)\n", dep.Path, dep.Version, dep.Sum)
+		if dep.Replace != nil {
+			r := dep.Replace
+			fmt.Printf("   replaced by %s@%s (%s)\n", r.Path, r.Version, r.Sum)
+		}
+	}
 
 	return nil
 }
