1e4d8e1999a904b16158dbb5ffc66fbec72048c7
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-04-27 14:55:13 -0700

    Add test

---
+++ internal/notes/todo_test.go
@@ -0,0 +1,29 @@
+package notes
+
+import (
+	"bytes"
+	"io"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBody(t *testing.T) {
+	r := body("testing", "123", time.Date(2012, 12, 12, 12, 12, 12, 0, time.UTC))
+	buf := &bytes.Buffer{}
+	if _, err := io.Copy(buf, r); err != nil {
+		t.Fatalf("Couldn't couldn't copy body: %s", err)
+	}
+
+	assert.Equal(t, `---
+title: "testing"
+date: "2012-12-12T12:12:12"
+tags: [ private, inbox ]
+guid: 123
+---
+
+ * testing
+
+`, buf.String())
+}
