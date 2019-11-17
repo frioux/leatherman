e12d9bb706d1e8a816d8badbfa12f86faa215adf
Arthur Axel 'fREW' Schmidt <git@frew.co>
2019-11-17 07:55:31 -0800

    Add basic online help

--- internal/notes/notes.go
+++ internal/notes/notes.go
@@ -16,6 +16,13 @@ type rule struct {
 // Rules has an ordered list of regexp+callback rules
 type Rules []rule
 
+func help(_ dropbox.Client) func(string, []twilio.Media) (string, error) {
+	return func(string, []twilio.Media) (string, error) {
+		// url is https://github.com/frioux/amygdala/blob/master/README.mdwn#commands
+		return "• help\n• inspire me\n• defer <m> til <t>\n• remind me <x> [at <t>|in <d>]\n• <todo>\nhttps://git.io/Jeojv", nil
+	}
+}
+
 // NewRules creates default rule set
 func NewRules(token string) (Rules, error) {
 	cl, err := dropbox.NewClient(dropbox.Client{Token: token})
@@ -23,6 +30,7 @@ func NewRules(token string) (Rules, error) {
 		return nil, err
 	}
 	return []rule{
+		{Regexp: regexp.MustCompile(`(?i)^\s*(?:commands|cmd|cmds)\s*$`), action: help(cl)},
 		{Regexp: regexp.MustCompile(`(?i)^\s*inspire\s+me\s*$`), action: inspireMe(cl)},
 		{Regexp: regexp.MustCompile(`(?i)^\s*remind\s+me\s*`), action: remind(cl)},
 		{Regexp: deferPattern, action: deferMessage(cl)},
