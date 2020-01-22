6705eda0270ceaa47144fbc4bde3a4a690847276
Arthur Axel 'fREW' Schmidt <git@frew.co>
2020-01-21 20:10:03 -0800

    Ensure ordered output for stable history

--- internal/tool/smlist/smList.go
+++ internal/tool/smlist/smList.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"sort"
 	"sync"
 	"time"
 
@@ -29,36 +30,41 @@ func Run(_ []string, _ io.Reader) error {
 	// 15s for the index
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
-	coffees, err := sweetmarias.AllCoffees(ctx)
+	urls, err := sweetmarias.AllCoffees(ctx)
 	if err != nil {
 		return fmt.Errorf("sweetmarias.AllCoffees: %w", err)
 	}
 
-	e := json.NewEncoder(os.Stdout)
+	coffees := make([]sweetmarias.Coffee, len(urls))
 
 	// 5m for all the details
 	ctx, cancel = context.WithTimeout(context.Background(), 5*time.Minute)
 	defer cancel()
-	for _, url := range coffees {
+	for i, url := range urls {
 		wg.Add(1)
 		tokens <- struct{}{}
-		url := url
+		i, url := i, url
 		go func() {
 			defer func() { wg.Done(); <-tokens }()
-			c, err := sweetmarias.LoadCoffee(ctx, url)
+			var err error
+			coffees[i], err = sweetmarias.LoadCoffee(ctx, url)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "sweetmarias.LoadCoffee: %s\n", err)
-				return
-			}
-			err = e.Encode(c)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "json.Encode: %s\n", err)
 				return
 			}
 		}()
 	}
 
 	wg.Wait()
+
+	sort.Slice(coffees, func(i, j int) bool { return coffees[i].URL < coffees[j].URL })
+
+	e := json.NewEncoder(os.Stdout)
+	for _, c := range coffees {
+		if err := e.Encode(c); err != nil {
+			return fmt.Errorf("json.Encode: %w", err)
+		}
+	}
 
 	return nil
 }
