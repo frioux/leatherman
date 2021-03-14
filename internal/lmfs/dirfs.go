487792f68ef0c21e5cdce8251d9432229664a6a0
Arthur Axel 'fREW' Schmidt <git@frew.co>
2021-03-14 13:30:19 -0700

    lmfs: add Watch to DirFS

--- internal/lmfs/dirfs.go
+++ internal/lmfs/dirfs.go
@@ -1,10 +1,14 @@
 package lmfs
 
 import (
+	"context"
+	"fmt"
 	"io/fs"
 	"os"
 	"path/filepath"
 	"runtime"
+
+	"github.com/fsnotify/fsnotify"
 )
 
 func OpenDirFS(d string) fs.FS {
@@ -43,4 +47,84 @@ func (fss dirFS) Create(f string) (FileWriter, error) {
 
 func (fss dirFS) Sub(d string) (fs.FS, error) {
 	return OpenDirFS(filepath.Join(string(fss), d)), nil
+}
+
+func addDir(watcher *fsnotify.Watcher, path string) error {
+	return filepath.WalkDir(path, func(path string, info fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if !info.IsDir() {
+			return nil
+		}
+
+		if err := watcher.Add(path); err != nil {
+			return fmt.Errorf("lmfs.dirFS: watcher.Add: %w", err)
+		}
+		return nil
+	})
+}
+
+func (fss dirFS) Watch(ctx context.Context, path string) (chan []fsnotify.Event, error) {
+	watchroot := filepath.Join(string(fss), path)
+	ch := make(chan []fsnotify.Event)
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, fmt.Errorf("fsnotify.NewWatcher: %w", err)
+	}
+
+	go func() {
+		<-ctx.Done()
+		watcher.Close()
+	}()
+
+	if err := addDir(watcher, watchroot); err != nil {
+		return nil, err
+	}
+
+	go func() {
+	LOOP:
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					fmt.Fprintln(os.Stderr, "watcher went away")
+					break LOOP
+				}
+
+				r, err := filepath.Rel(watchroot, event.Name)
+				if err != nil {
+					panic(err)
+				}
+				ch <- []fsnotify.Event{{Op: event.Op, Name: r}}
+
+				if event.Op&fsnotify.Create == fsnotify.Create {
+					stat, err := os.Stat(event.Name)
+					if err != nil {
+						if os.IsNotExist(err) {
+							continue LOOP
+						}
+						fmt.Fprintf(os.Stderr, "Couldn't stat created thing: %s\n", err)
+					} else if stat.IsDir() {
+						if err := addDir(watcher, event.Name); err != nil {
+							fmt.Fprintf(os.Stderr, "failed to watch %s: %s\n", event.Name, err)
+						}
+					}
+				}
+
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					fmt.Fprintln(os.Stderr, "watcher went away")
+					break LOOP
+				}
+				fmt.Fprintln(os.Stderr, "watcher error:", err)
+
+			case <-ctx.Done():
+				break LOOP
+			}
+		}
+	}()
+
+	return ch, nil
 }
