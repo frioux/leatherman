18697edd69f46d6bd8ee77bc759f70a343fbb7dc
Arthur Axel 'fREW' Schmidt <git@frew.co>
2021-05-23 22:36:46 -0700

    add test for print/println

---
+++ noprintln_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"os/exec"
+	"testing"
+)
+
+func TestNoPrintln(t *testing.T) {
+	cmd := exec.Command("git", "grep", "--quiet", "--perl-regexp", `\bpr[i]nt(ln)?\(`, "*.go")
+	err := cmd.Run()
+	var e *exec.ExitError
+
+	if errors.As(err, &e) && e.ExitCode() != 0 {
+		return
+	}
+
+	if err != nil {
+		t.Errorf("unexpected error from git grep: %s\n", err)
+	} else {
+		b := &bytes.Buffer{}
+		t.Errorf("git grep found the forbidden println:")
+		cmd := exec.Command("git", "grep", "--perl-regexp", `\bpr[i]nt(ln)?\(`, "*.go")
+		cmd.Stdout = b
+		cmd.Stderr = b
+		cmd.Run()
+		t.Error(b.String())
+	}
+}
